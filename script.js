249e506e4cd4a0d6d95d1a9a5ce9792d17aa311e
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-04-24 18:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const person = {
 };
 let team=players;
 let team1=players;
-person cap1= new person;
+let cap1=person;
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
